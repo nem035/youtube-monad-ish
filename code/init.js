4a2fbaaff05f80ae2efc51080a7e259b782df56a
nem035 <n.stojanovic035@gmail.com>
2016-10-02 07:48:43 -0400

    removed debugger

--- code/init.js
+++ code/init.js
@@ -3,6 +3,5 @@ const app = require('./app');
 fetch('https://youtube-shuffle-ejpiaj.herokuapp.com')
   .then((res) => res.json())
   .then(({ apiKey }) => {
-    debugger;
     app.start(apiKey);
   });
--- dist/bundle.js
+++ dist/bundle.js
@@ -386,7 +386,6 @@ const app = require('./app');
 fetch('https://youtube-shuffle-ejpiaj.herokuapp.com')
   .then((res) => res.json())
   .then(({ apiKey }) => {
-    debugger;
     app.start(apiKey);
   });
 
